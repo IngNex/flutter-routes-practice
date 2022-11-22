e5c7e6b1a123087fe824f492cbfca6cbd71cf581
IngNex <ingnexmaicol14@gmail.com>
2022-11-22 10:41:53 -0500

    flutter pub routes

---
+++ README.md
@@ -0,0 +1,4 @@
+# FLUTTER 3.0
+# Practice Flutter
+
+## Practice flutter with dart, components and models
---
+++ android/app/src/main/kotlin/com/example/flutter_ecommerce_nex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ecommerce_nex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
